887e9f966567578300ac411c9cd6edaa9e9fbf17
Mustafa Kara <mkara@opsheaven.space>
2024-03-03 22:19:38 +0300

    Migrate to sshfs from cifs
    Signed-off-by: Mustafa Kara <mkara@opsheaven.space>

--- .mk/state_mount.mk
+++ .mk/state_mount.mk
@@ -1,4 +1,4 @@
-.PHONY: state/umount
-state/umount:
-	@sudo umount ./state
-	@rm -rf ./state
+.PHONY: state/mount
+state/mount:
+	@mkdir -p state
+	@sshfs u393136@u393136.your-storagebox.de:/terraform ./state -o ssh_command="sshpass -p $(shell pass storagebox) ssh"
--- .mk/state_umount.mk
+++ .mk/state_umount.mk
@@ -1,4 +1,4 @@
-.PHONY: state/mount
-state/mount:
-	@mkdir -p state
-	@sudo mount -t cifs //u393136.your-storagebox.de/backup/terraform ./state -o user=u393136,password=$(shell pass storagebox),noperm
+.PHONY: state/umount
+state/umount:
+	@umount ./state
+	@rm -rf ./state
